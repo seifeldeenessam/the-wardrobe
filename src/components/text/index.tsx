8650e4c9e6e6b81d285985d3c2eb59b81b293b5b
Seif Eldeen Essam <seifeldeenessam01@gmail.com>
2023-12-09 20:26:21 +0200

    Added optional className prop to `Text` component

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -5,18 +5,19 @@ type Variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
 type Props = {
 	variant: Variant;
 	content: string;
+	className?: string;
 };
 
-const Text = ({ variant, content }: Props) => {
+const Text = ({ variant, content, className }: Props) => {
 	if (!getText(content)) return null;
 
-	if (variant === 'h1') return <h1>{getText(content)}</h1>;
-	if (variant === 'h2') return <h2>{getText(content)}</h2>;
-	if (variant === 'h3') return <h3>{getText(content)}</h3>;
-	if (variant === 'h4') return <h4>{getText(content)}</h4>;
-	if (variant === 'h5') return <h5>{getText(content)}</h5>;
-	if (variant === 'h6') return <h6>{getText(content)}</h6>;
-	if (variant === 'p') return <p>{getText(content)}</p>;
+	if (variant === 'h1') return <h1 className={className}>{getText(content)}</h1>;
+	if (variant === 'h2') return <h2 className={className}>{getText(content)}</h2>;
+	if (variant === 'h3') return <h3 className={className}>{getText(content)}</h3>;
+	if (variant === 'h4') return <h4 className={className}>{getText(content)}</h4>;
+	if (variant === 'h5') return <h5 className={className}>{getText(content)}</h5>;
+	if (variant === 'h6') return <h6 className={className}>{getText(content)}</h6>;
+	if (variant === 'p') return <p className={className}>{getText(content)}</p>;
 
 	return null;
 };
